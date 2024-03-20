4ceb0ee76b889884ac414ab6c728e19865fe2154
osakac <shinmoriof@gmail.com>
2024-03-20 17:21:07 +0400

    init

---
+++ src/db.ts
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: "Apple BYZ S852I",
+    image: "/images/iphone13.png",
+    price: 2_927,
+    rating: 4.7,
+  },
+  {
+    id: 2,
+    name: "Apple EarPods",
+    image: "/images/iphone13.png",
+    price: 2_327,
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: "Apple EarPods",
+    image: "/images/iphone13.png",
+    price: 2_327,
+    rating: 4.5,
+  },
+  {
+    id: 4,
+    name: "Apple BYZ S852I",
+    image: "/images/iphone13.png",
+    price: 2_927,
+    rating: 4.7,
+  },
+  {
+    id: 5,
+    name: "Apple EarPods",
+    image: "/images/iphone13.png",
+    price: 2_327,
+    rating: 4.5,
+  },
+  {
+    id: 6,
+    name: "Apple EarPods",
+    image: "/images/iphone13.png",
+    price: 2_327,
+    rating: 4.5,
+  },
+]
