265550bf2a99d4601fd8e59b91a05ffb4eae815d
osakac <shinmoriof@gmail.com>
2024-03-21 11:08:31 +0400

    add home and cart routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Suspense, lazy } from "react"
+import { Route, Routes } from "react-router-dom"
+import { MainLayout } from "./layouts/MainLayout"
+
+const Home = lazy(() => import("./pages/Home/Home"))
+const Cart = lazy(() => import("./pages/Cart/Cart"))
+
 function App() {
-  return <></>
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route
+          index
+          element={
+            <Suspense fallback={null}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <Suspense fallback={null}>
+              <Cart />
+            </Suspense>
+          }
+        />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
