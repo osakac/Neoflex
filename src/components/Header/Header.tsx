b21a6b266989fc348d3075649bbc2778c9f4a9f3
osakac <shinmoriof@gmail.com>
2024-03-22 16:41:32 +0400

    add counts on icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,22 @@
 import CartIcon from "@/assets/svg/cart.svg?react"
 import HeartIcon from "@/assets/svg/heart.svg?react"
 import { Link } from "react-router-dom"
-import classes from "./Header.module.scss"
+import cl from "./Header.module.scss"
 
 export const Header = () => {
   return (
-    <header className={classes.header}>
-      <Link to={"/"} className={classes.logo}>
+    <header className={cl.header}>
+      <Link to={"/"} className={cl.logo}>
         QPICK
       </Link>
 
-      <div className={classes.panel}>
-        <button>
-          <HeartIcon />
-        </button>
-        <Link to={"/cart"}>
+      <div className={cl.panel}>
+        <Link to={"/"} className={cl.favoritesBtn}>
+          <span>0</span>
+          <HeartIcon className={cl.heartIcon} />
+        </Link>
+        <Link to={"/cart"} className={cl.cartBtn}>
+          <span>0</span>
           <CartIcon />
         </Link>
       </div>
