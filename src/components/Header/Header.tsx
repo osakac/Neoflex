082b8784b6c687975cc8244fe2172ab329218005
osakac <shinmoriof@gmail.com>
2024-03-25 13:23:16 +0400

    add products count on cart icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,14 @@
 
 import CartIcon from "@/assets/svg/cart.svg?react"
 import HeartIcon from "@/assets/svg/heart.svg?react"
+import { RootState } from "@/store"
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import cl from "./Header.module.scss"
 
 export const Header = () => {
+  const cartCount = useSelector((state: RootState) => state.cart.cart).length
+
   return (
     <header className={cl.header}>
       <Link to={"/"} className={cl.logo}>
@@ -18,7 +22,7 @@ export const Header = () => {
           <HeartIcon className={cl.heartIcon} />
         </Link>
         <Link to={"/cart"} className={cl.cartBtn}>
-          <span>0</span>
+          <span>{cartCount}</span>
           <CartIcon />
         </Link>
       </div>
