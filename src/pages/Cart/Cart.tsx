4aed179148f4b4524bf442b1d78407c24d5c0334
osakac <shinmoriof@gmail.com>
2024-03-25 13:24:20 +0400

    improve cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,27 @@
+import CartList from "@/components/Cart/CartList/CartList"
+import MakingOrder from "@/components/Cart/MakingOrder/MakingOrder"
+import { RootState } from "@/store"
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+import cl from "./Cart.module.scss"
+
 const Cart = () => {
-  return <div>Cart</div>
+  const cart = useSelector((state: RootState) => state.cart.cart)
+  const cartPrice = useMemo(() => {
+    return cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+  }, [])
+
+  return (
+    <>
+      <h2 className={cl.title}>Корзина</h2>
+
+      <div className={cl.wrapper}>
+        <CartList products={cart} />
+
+        <MakingOrder cartPrice={cartPrice} />
+      </div>
+    </>
+  )
 }
 
 export default Cart
