11438d78b06c4c146d8b0038a49c65faea5bb624
osakac <shinmoriof@gmail.com>
2024-03-25 16:52:08 +0400

    clear unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App.tsx"
 import CartProvider from "./context/cartContext.tsx"
 import "./index.scss"
-import store from "./store"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <React.StrictMode>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 )
