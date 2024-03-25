fe27a655ff512a0280c0bfc6af90587f035db463
osakac <shinmoriof@gmail.com>
2024-03-25 13:39:46 +0400

    add empty cart case

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -3,13 +3,24 @@ import MakingOrder from "@/components/Cart/MakingOrder/MakingOrder"
 import { RootState } from "@/store"
 import { useMemo } from "react"
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 import cl from "./Cart.module.scss"
 
 const Cart = () => {
   const cart = useSelector((state: RootState) => state.cart.cart)
   const cartPrice = useMemo(() => {
     return cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
-  }, [])
+  }, [cart])
+
+  if (!cart.length)
+    return (
+      <>
+        <h2 className={cl.emptyTitle}>Корзина пуста</h2>
+        <Link to={"/"} className={cl.emptyBtn}>
+          Перейти в каталог
+        </Link>
+      </>
+    )
 
   return (
     <>
