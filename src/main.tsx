bdc7f5e4122c423005198abbef2e3bca1b417289
osakac <shinmoriof@gmail.com>
2024-03-25 12:37:56 +0400

    configure redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App.tsx"
 import "./index.scss"
+import store from "./store"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 )
