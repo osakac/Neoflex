9b5ad4482c3cd3a88155b2369e6e99d19b86f88d
osakac <shinmoriof@gmail.com>
2024-03-26 14:57:27 +0400

    fix

--- src/components/Product/ProductQuantity/ProductQuantity.tsx
+++ src/components/Product/ProductQuantity/ProductQuantity.tsx
@@ -1,26 +1,30 @@
 import MinusIcon from "@/assets/svg/minus.svg?react"
 import PlusIcon from "@/assets/svg/plus.svg?react"
-// import { CartContext } from "@/context/cartContext"
-// import { useContext } from "react"
+import { CartContext } from "@/context/cartContext"
+import { useContext } from "react"
 import cl from "./ProductQuantity.module.scss"
 
 interface Props {
   id: number
   quantity: number
-  increaseFn: () => void
-  decreaseFn: () => void
 }
 
-const ProductQuantity = ({ quantity, increaseFn, decreaseFn }: Props) => {
-  // const { updateQuantity } = useContext(CartContext)
+const ProductQuantity = ({ id, quantity }: Props) => {
+  const { updateQuantity } = useContext(CartContext)
 
   return (
     <div className={cl.count}>
-      <button onClick={decreaseFn} className={cl.minus}>
+      <button
+        onClick={() => updateQuantity(id, quantity - 1)}
+        className={cl.minus}
+      >
         <MinusIcon />
       </button>
       <span>{quantity}</span>
-      <button onClick={increaseFn} className={cl.plus}>
+      <button
+        onClick={() => updateQuantity(id, quantity + 1)}
+        className={cl.plus}
+      >
         <PlusIcon />
       </button>
     </div>
