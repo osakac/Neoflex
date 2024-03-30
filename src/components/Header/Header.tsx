5ae6d636f83e794425b5cb97efce07f0045a2626
osakac <shinmoriof@gmail.com>
2024-03-30 23:17:17 +0400

    refactor from context to redux

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,14 @@
 
 import CartIcon from "@/assets/svg/cart.svg?react"
 import HeartIcon from "@/assets/svg/heart.svg?react"
-import { CartContext } from "@/context/cartContext"
-import { useContext, useMemo } from "react"
+import { useMemo } from "react"
 import { Link } from "react-router-dom"
 import cl from "./Header.module.scss"
+import { useSelector } from "react-redux"
+import { RootState } from "@/redux"
 
 const Header = () => {
-  const { cart } = useContext(CartContext)
+  const cart = useSelector((state: RootState) => state.cart.cart)
 
   const cartCount = useMemo(() => {
     return cart.length
@@ -16,16 +17,25 @@ const Header = () => {
 
   return (
     <header className={cl.header}>
-      <Link to={"/"} className={cl.logo}>
+      <Link
+        to={"/"}
+        className={cl.logo}
+      >
         QPICK
       </Link>
 
       <div className={cl.panel}>
-        <Link to={"/"} className={cl.favoritesBtn}>
+        <Link
+          to={"/"}
+          className={cl.favoritesBtn}
+        >
           <span>0</span>
           <HeartIcon className={cl.heartIcon} />
         </Link>
-        <Link to={"/cart"} className={cl.cartBtn}>
+        <Link
+          to={"/cart"}
+          className={cl.cartBtn}
+        >
           <span>{cartCount}</span>
           <CartIcon />
         </Link>
